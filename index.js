b2fbc47021cb7fc27a83352283b9b1c31a5e6809
Aiden FatFish <aidenfavish@gmail.com>
2020-10-12 01:11:19 +0000

    new

--- index.js
+++ index.js
@@ -23,53 +23,27 @@ client.on('ready', () => {
 
 client.on('message', message => {
   user = message.mentions.users.first()
-  if (message.content == 'remember') {
-    message.channel.send("remember satan\nremember timmy\nremember satan fetus\nremember all of the fallen bots")
+  if (message.content == 'welcome' || message.content == 'Welcome') {
+    message.channel.send(`Welcome to ${message.guild.name}`)
   }
 });
 
 
 
-client.on('message', message => {
-  // If the message is "how to embed"
-  if (message.content === 'fwaygfwryfg') {
-    // We can create embeds using the MessageEmbed constructor
-    // Read more about all that you can do with the constructor
-    // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
-    const embed = new MessageEmbed()
-      // Set the title of the field
-      .setTitle('The Birth')
-      // Set the color of the embed
-      .setColor(0xff0000)
-      // Set the main content of the embed
-      .setDescription('@everyone please gather around and whitness the great birth of baby Satan');
-    // Send the embed to the same channel as the message
-    message.channel.send(embed);
-    message.channel.send(`Baby Satan is being born because no one has killed him yet his counter will reset try to kill him faster`);
-    
-  }
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'anouncments');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+  member.send(`Welcome to ${member.guild.name}`)
 });
-
 client.on('message', message => {
-  // If the message is "ping"
-  if (message.content === 'I hate you') {
-    
-
-    // Send "pong" to the same channel
-    message.channel.send('What? Why?');
-    message.channel.send('I did not do anything');
-    message.channel.send('you big bully');
-    message.channel.send('YOUR A JERK AND A BIG BULLY!'); 
-    message.channel.send('your just jelous of my face');
-    message.channel.send('Look at me now I am crying'); 
-  }
-  if (message.content === "I hate you".toLowerCase()){
-    message.channel.send('What? Why?');
-    message.channel.send('I did not do anything');
-    message.channel.send('you big bully');
-    message.channel.send('YOUR A JERK AND A BIG BULLY!'); 
-    message.channel.send('your just jelous of my face');
-    message.channel.send('Look at me now I am crying');
+  // If the message is "what is my avatar"
+  if (message.content === '^avatar') {
+    // Send the user's avatar URL
+    message.reply(message.author.displayAvatarURL());
   }
 });
 
